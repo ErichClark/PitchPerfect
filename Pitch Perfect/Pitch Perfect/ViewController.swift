50f3c70bde86e83769b6fc9852278baaf7072a81
ErichClark <erich.clark@gmail.com>
2018-02-25 21:16:00 -0500

    Added viewWillApear print statement

--- Pitch Perfect/Pitch Perfect/ViewController.swift
+++ Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -17,6 +17,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
